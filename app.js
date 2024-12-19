19e55a2b7212031af735ae7d948d1a98af1bffad
Jerome <jbawa16@oakgrove.school>
2024-12-19 12:19:35 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Graph from "./main/src/components/Graph";
+
+function App() {
+  return (
+      <div className="App">
+          <h1>Storybranch Prototype</h1>
+            <Graph />
+      </div>
+  );
+}
+
+export default App;
