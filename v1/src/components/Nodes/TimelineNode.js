ff332bbae864acdfddff578535a9999d148c1e8d
Jerome <jbawa16@oakgrove.school>
2024-12-20 22:26:41 +0000

    Changed Node system to use LinkedTimeline

--- v1/src/components/Nodes/TimelineNode.js
+++ v1/src/components/Nodes/TimelineNode.js
@@ -1,7 +1,9 @@
-class Node {
+class TimelineNode {
     constructor(data, name = "ARC", x = 0, y = 0) {
         this.name = name;         // Name of the Node
-        this.data = data;         // Instance of Data class
+        this.data = data;          // Instance of Data class
+        this.prev = null;         // Previous Node in the timeline
+        this.next = null;         // Next Node in the timeline
         this.branches = [];       // Array of child Nodes (branches)
         this.x = x;               // X position of the Node
         this.y = y;               // Y position of the Node
@@ -10,7 +12,13 @@ class Node {
     addBranch(node) {
         this.branches.push(node); // Add a new branch to this node
     }
+    setPrev(node) {
+        this.prev = node; // Set the previous node
+    }
+    setNext(node) {
+        this.next = node; // Set the next node
+    }
 }
 
-export default Node;
+export default TimelineNode;
 
